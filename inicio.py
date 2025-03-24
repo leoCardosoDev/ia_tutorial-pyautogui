edeaf237558532563a17c2c9099bf2184797d6ed
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-24 19:37:51 -0300

    feat: first script

--- inicio.py
+++ inicio.py
@@ -1,4 +1,10 @@
 import pyautogui
 
 pyautogui.moveTo(100,100, duration=1)
+pyautogui.moveTo(200,200, duration=2)
+pyautogui.moveTo(240,310, duration=3)
 
+pyautogui.click(240, 310)
+
+pyautogui.write('Hello World!', interval=0.25)
+pyautogui.press('enter')
