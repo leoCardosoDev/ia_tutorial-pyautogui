414bfcd768e2f2d88ea879c8df5ee41783114c96
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-03-28 10:13:01 -0300

    feat: add htl basic

--- web_scraping/local_scrap.py
+++ web_scraping/local_scrap.py
@@ -4,4 +4,16 @@ with open("index.html", "r") as file:
     content = file.read()
 
 ex = BeautifulSoup(content, "lxml")
-print(ex.prettify())
+# print(ex.prettify())
+print(ex.title)
+print(ex.find("p"))
+print(ex.find_all("p"))
+print(ex.find_all("p")[0])
+print(ex.find_all("p")[0].text)
+
+print('--__--' * 20)
+print(ex.find_all(class_="title"))
+
+tags = ex.find_all(class_="title")
+for tag in tags:
+    print(tag)
